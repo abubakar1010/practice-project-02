a760f411ec441fd75ac514e9634c43aa06d3ad78
Abubakar1010 <abubakar850772@gmail.com>
2024-02-16 02:03:28 +0600

    finished first step

---
+++ script/main.js
@@ -0,0 +1,9 @@
+
+const enterButton = document.getElementById('enterGameButton')
+
+//* Enter the Game
+
+enterButton.addEventListener('click', function() {
+
+    hideAndShowElement("homePage","playGround")
+})
---
+++ script/utility.js
@@ -0,0 +1,16 @@
+
+//* step-01: hide home page and show play ground
+
+function hideAndShowElement(hiddenElementId,showElementId){
+
+    const hide = document.getElementById(hiddenElementId).classList.add('hidden')
+
+    const show = document.getElementById(showElementId).classList.remove('hidden')
+}
+
+//* step-02: generate random numbers and random alphabets 
+
+function getRandomAlphabets(){
+
+    
+}
